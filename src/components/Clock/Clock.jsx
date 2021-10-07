a0d30ac4b68ce6831ff33d554c3a70de30853c47
Cristian Correa <cristiancorrea85@gmail.com>
2021-10-07 00:22:46 -0300

    prueba Clock

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -4,13 +4,13 @@ import './Clock.scss';
 export const Clock = () => {
     const [time, setTime] = useState (new Date());
 
-    const updateTime = () =>{
+    const handleTime = () =>{
         setTime( new Date())
         console.log(time)
     }
 
     return(
-        <div onClick={updateTime}>
+        <div onClick={handleTime} className="container">
             <h3>{time.toLocaleString()}</h3>
         </div>
     )
